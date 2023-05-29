bab6ce5b50f4baeaf67da47ef20dc4a6052c3f98
Eric Waithaka <74556293+eric-waithaka@users.noreply.github.com>
2023-05-29 23:16:52 +0300

    First commit

---
+++ client/src/App.js
@@ -0,0 +1,57 @@
+import React, { useEffect, useState } from 'react';
+
+function App() {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('http://localhost:8000/products');
+        const jsonData = await response.json();
+        setData(jsonData);
+        setLoading(false);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div>
+      <h1>Inventory</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Product Name</th>
+            <th>Quantity</th>
+            <th>Supplier</th>
+            <th>Patient Name</th>
+            <th>Patient Telephone</th>
+            <th>Patient Hospital</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map(item => (
+            <tr key={item._id}>
+              <td>{item.product_name}</td>
+              <td>{item.quantity}</td>
+              <td>{item.supplier}</td>
+              <td>{item.patient_name}</td>
+              <td>{item.patient_telephone}</td>
+              <td>{item.patient_hospital}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default App;
